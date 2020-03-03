7d0cddb7c123f7063cef06659302e351d1a6ba83
Matko <matkomatic59@gmail.com>
2020-03-03 11:16:04 +0100

    ready for peno drinkathon

---
+++ config_template.php
@@ -0,0 +1,21 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "dbname";
+
+// Create connection
+$db = new mysqli($servername, $username, $password, $dbname);
+
+
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+session_start();
