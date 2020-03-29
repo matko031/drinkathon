b016d00f5b9189839d5f650bd32f37e435298f83
matko031 <matkomatic59@gmail.com>
2020-03-29 15:00:55 +0200

    removed main.py with sensitive data

---
+++ flask_app/config_template.py
@@ -0,0 +1,9 @@
+
+#Config MySQL
+configuration = {
+    'MYSQL_HOST' = 'host',
+    'MYSQL_USER'] = 'username',
+    'MYSQL_PASSWORD'] = 'password',
+    'MYSQL_DB'] = 'database_name',
+    'MYSQL_CURSORCLASS'] = 'CursorClass',
+    }
