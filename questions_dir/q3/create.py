2dd7d90e06d9f75539fa1f070cd3abfd093ce156
matko031 <matkomatic59@gmail.com>
2020-11-28 20:47:36 +0100

    q3 done

--- questions_dir/q3/create.py
+++ questions_dir/q3/create.py
@@ -12,20 +12,27 @@ def output_sum(n):
 
 
 with open('input', 'w') as f:
+    f.write(str(N) + "\n")
     list_len = random.randint(1, 15)
     for i in range(N):
         lis = []
         list_len = random.randint(1,100)
         for j in range(list_len):
             lis.append(random.randint(1, 200))
+        if random.randint(1, 100) > 50:
+            lis.append(lis[0])
         f.write(str(lis) + "\n")
 
 
-"""
+
 with open('input', 'r') as f_in:
     f_in.readline()
     with open('output', 'w') as f_out:
-        for i in range(N-1):
-            n = int(f_in.readline())
-            f_out.write(output_sum(n))
-"""
+        for i in range(N):
+            l = eval(f_in.readline())
+            f_out.write("given list is " + str(l) + "\n")
+            if l[0] == l[-1]:
+                f_out.write('true\n')
+            else:
+                f_out.write('false\n')
+
