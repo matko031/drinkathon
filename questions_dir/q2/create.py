a21fed2d8b76c567f5d3b399b9b69181c44e5ac0
matko031 <matkomatic59@gmail.com>
2020-11-28 20:15:06 +0100

    q2 ready

---
+++ questions_dir/q2/create.py
@@ -0,0 +1,27 @@
+import random
+N = 100
+
+
+
+def output_sum(n):
+    res = ""
+    s = 0
+    for i in range(n):
+        s += i
+        res += "{} {}\n".format(i, s)
+    return res
+
+
+with open('input', 'w') as f:
+    for i in range(N):
+        n = random.randint(1, 200)
+        f.write(str(n) + "\n")
+
+
+
+with open('input', 'r') as f_in:
+    f_in.readline()
+    with open('output', 'w') as f_out:
+        for i in range(N-1):
+            n = int(f_in.readline())
+            f_out.write(output_sum(n))
