a990732018f7eec4dee0466160de9f3a582e373e
matko031 <matkomatic59@gmail.com>
2020-11-28 20:35:29 +0100

    fixed q2

--- questions_dir/q3/create.py
+++ questions_dir/q3/create.py
@@ -2,19 +2,22 @@ import random
 N = 100
 
 
-
 def output_sum(n):
     res = ""
-    previous = 0
+    s = 0
     for i in range(n):
-        res += "{} {}\n".format(i, i+previous)
-        previous=i
+        s += i
+        res += "{} {}\n".format(i, s)
     return res
 
 
 with open('input', 'w') as f:
+    list_len = random.randint(100)
     for i in range(N):
-        n = random.randint(1, 200)
+        lis = []
+        list_len = random.randint(100)
+        for j in range(list_len):
+            lis.append(random.randint(1, 200))
         f.write(str(n) + "\n")
 
 
