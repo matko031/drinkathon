28c3f74ecb7cd3ed83b948cf2bbbd3d332433f4e
matko031 <matkomatic59@gmail.com>
2020-11-28 20:41:59 +0100

    fixed q2 again

--- questions_dir/q2/create.py
+++ questions_dir/q2/create.py
@@ -13,6 +13,7 @@ def output_sum(n):
 
 
 with open('input', 'w') as f:
+    f.write(str(N) + "\n")
     for i in range(N):
         n = random.randint(1, 200)
         f.write(str(n) + "\n")
@@ -22,6 +23,6 @@ with open('input', 'w') as f:
 with open('input', 'r') as f_in:
     f_in.readline()
     with open('output', 'w') as f_out:
-        for i in range(N-1):
+        for i in range(N):
             n = int(f_in.readline())
-            f_out.write(output_sum(n))
+            f_out.write(output_sum(n)+ "\n")
--- questions_dir/q3/create.py
+++ questions_dir/q3/create.py
@@ -12,19 +12,20 @@ def output_sum(n):
 
 
 with open('input', 'w') as f:
-    list_len = random.randint(100)
+    list_len = random.randint(1, 15)
     for i in range(N):
         lis = []
-        list_len = random.randint(100)
+        list_len = random.randint(1,100)
         for j in range(list_len):
             lis.append(random.randint(1, 200))
-        f.write(str(n) + "\n")
+        f.write(str(lis) + "\n")
 
 
-
+"""
 with open('input', 'r') as f_in:
     f_in.readline()
     with open('output', 'w') as f_out:
         for i in range(N-1):
             n = int(f_in.readline())
             f_out.write(output_sum(n))
+"""
